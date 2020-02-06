b6891c6439e85a7f2ef6903fb44c1c78e4953884
aeotero <popal9193@gmail.com>
2020-02-06 20:38:08 -0300

    trabajo levantado tras reset forzoso

--- clasesYfunciones.py
+++ clasesYfunciones.py
@@ -5,6 +5,31 @@ import logging
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
 def dormir():
+    logging.info("empezando desde funcion")
+    time.sleep(1)
+    logging.info("finalizando desde funcion")
+
+t1 = threading.Thread(target=dormir , name= "tread desde funcion")
+t1.start()
+
+class Unthread(threading.Thread): 
+    #Entre los parentesis va la superclase de la que hereda.
+    def __init__(self):
+        super().__init__() # inicializa el init de la super clase
+        self.name= "threadClase"
+
+    def run(self):
+        logging.info("empezando desde clase")
+        time.sleep(1)
+        logging.info("finalizando desde clase")
+
+t2 = Unthread() # Para hacer un objeto nuevo es RECOMENDADO copiar y pegar nombre de la clase, XQ ? no se , pero anda. 
+t2.start() 
+#Esto se ejecuta llamando al run de la clase .Si no hay run , no hace nada.
+
+"""
+ESTE ES EL CODIGO DEL PROFE, HACE LO MISMO QUE EL MIO
+def dormir():
     logging.info('Empenzado')
     time.sleep(1)
     logging.info('Finalizado')
@@ -28,3 +53,5 @@ threadObj = UnThread()
 logging.info('Lanzando los threads')
 threadFunc.start()
 threadObj.start()
+
+"""
