5554c2b8f8d5ad6efdac0dcb4b325a5878c89ca8
aeotero <popal9193@gmail.com>
2020-02-06 19:10:58 -0300

    termidao el repaso con un erros , sigo con los ejercicios

--- SegundaClaseRepaso.py
+++ SegundaClaseRepaso.py
@@ -2,5 +2,27 @@ import threading
 import time
 import logging
 
-logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INF
+logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
+def dormir():
+    logging.info("empezando desde funcion")
+    time.sleep(1)
+    logging.info("finalizando desde funcion")
+
+t1 = threading.Thread(target=dormir , name= "tread desde funcion")
+t1.start()
+
+class Unthread(threading.Thread): 
+    #Entre los parentesis va la superclase de la que hereda.
+    def __init__(self):
+        super().__init__() # inicializa el init de la super clase
+        self.name= "threadClase"
+
+    def run(self):
+        logging.info("empezando desde clase")
+        time.sleep(1)
+        logging.info("finalizando desde clase")
+
+t2 = UnThread() # no me lo levanta , dice que no esta definido , como si hubuese que definirlo
+t2.start() 
+#Esto se ejecuta llamando al run de la clase .Si no hay run , no hace nada.
