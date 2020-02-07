98399955883cda924bf822b1141d354827fa8c83
aeotero <popal9193@gmail.com>
2020-02-06 21:15:29 -0300

    trabajo a medio hacer

--- operaNoConm.py
+++ operaNoConm.py
@@ -5,4 +5,26 @@ import logging
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
 
+numero = 0
 
+def sumarUno():
+    global numero
+    numero += 1
+
+def multiplicarPorDos():
+    global numero
+    numero = numero * 2
+
+t1 = threading.Thread(target= sumarUno , name= "tread de suma")
+logging.info
+t1.start()
+print (numero)
+t2 = threading.Thread(target= multiplicarPorDos , name= "tread de multiplicacion")
+t2.start()
+t3 = threading.Thread(target= multiplicarPorDos , name= "tread de multiplicacion")
+t4 = threading.Thread(target= multiplicarPorDos , name= "tread de multiplicacion")
+t5 = threading.Thread(target= multiplicarPorDos , name= "tread de multiplicacion")
+t3.start()
+t4.start()
+t5.start()
+print (numero)
